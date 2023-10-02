ae2e1ed22076018da2cfd3ec254c4941bff64ade
bardlinga <balin6546@oslomet.no>
2023-10-02 22:51:41 +0200

    pi done

--- src/Kontrollstrukturer/VanskeligereOppg1.java
+++ src/Kontrollstrukturer/VanskeligereOppg1.java
@@ -5,16 +5,6 @@ import java.util.Scanner;
 
 public class VanskeligereOppg1 {
 
-    /*
-    Beregn Pi med 10 desimalers nøyaktighet. Pi med 12 desimaler er 3.14159265358. Bruk en løkke til å beregne pi ved hjelp av denne rekken:
-
-    Pi=4-4/3+4/5-4/7+4/9-4/11+....
-
-    Lag en innlesning av antall ledd i rekken (omganger i løkken) slik at det er mulig å variere hvor mange som trengs for å oppnå en presisjon på 10 siffer.
-
-    Tips: Bruk long isteden for int for å ha mulighet for stort nok tall i løkken.
-    */
-
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
